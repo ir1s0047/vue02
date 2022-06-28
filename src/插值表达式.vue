11eac104e0dffe6f9b4afa6fa90e3d4eba1c3ffd
ir1s0047 <214749466@qq.com>
2022-06-28 19:56:22 +0800

    插值表达式

---
+++ src/插值表达式.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>{{msg}}</h1>
+    <h2>{{obj.author}}</h2>
+    <h3>{{obj.class}}</h3>
+    <h3>{{obj.time>=120?"长片":"短片"}}</h3>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      msg:"奇异博士2",
+      obj:{
+        author:"漫威影业",
+        class:"恐怖片",
+        time:"130"
+      }
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
