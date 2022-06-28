638e4a88551ddf58941af923c25231b6207be29e
ir1s0047 <214749466@qq.com>
2022-06-28 21:18:25 +0800

    作业提交

---
+++ src/求数组中总和.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <span>{{sum()}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+    data () {
+      return {
+      
+      }
+    },
+    methods: {
+      sum(){
+        let initialValue = 0
+        let sum = [{a:100}, {a: 20}, {a:-10}].reduce((pre,cur)=>{
+          return pre+cur.a
+        },initialValue)
+       console.log(sum);
+      
+      },
+
+    }
+}
+</script>
+
+<style>
+
+</style>
